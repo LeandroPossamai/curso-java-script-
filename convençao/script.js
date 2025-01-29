39effda3cd97e418c4dca1a14d2b0420cc6e9af4
Leandro Possamai <124197652+LeandroPossamai@users.noreply.github.com>
2025-01-29 10:32:28 -0300

    aprendendo convençao

--- convençao/script.js
+++ convençao/script.js
@@ -1,14 +1,3 @@
-let nome = "jandro";
+let Nome = "leandro";
 
-console.log(nome);
-console.log(`o meu nome e ${nome}`);
-
-let laranjas = 5;
-
-console.log(laranjas * laranjas); // 25
-
-var name = "leandro";
-const ip = "170.0.2.21";
-
-console.log(name);
-console.log(ip);
+let seuNome = "leandro";
