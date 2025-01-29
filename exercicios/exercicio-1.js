f20b2db548b4a03578e561bc8533a14b57d5b6da
Leandro Possamai <124197652+LeandroPossamai@users.noreply.github.com>
2025-01-29 09:14:47 -0300

    operaçoes aritmeticas

--- exercicios/exercicio-1.js
+++ exercicios/exercicio-1.js
@@ -1,3 +1,9 @@
 console.log(1 > 5);
 console.log(4 <= 10);
-console.log(10 != 7);
+console.log("leandro" != "jorge");
+
+console.log(3 == 3 && 4 > 1);
+console.log(4 <= 2 || "leandro" == "jorge");
+console.log(!(3 == 3));
+
+console.log(3 - 3);
