7fa985128e2e8027b17cecd179349c9952b038bf
Leandro Possamai <124197652+LeandroPossamai@users.noreply.github.com>
2025-02-03 10:20:24 -0300

    metodo simples e metodo mas dificil de usar arrow function

---
+++ arrow-function-2/script.js
@@ -0,0 +1,9 @@
+const raizQuadrada = (x) => {
+  return x * x;
+}; // METODO SIMPLES DE ESCREVER UMA ARROW FUNCTION
+
+const raizQuadrada2 = (n) => n * n;
+// METODO MAS COMPLEXO DE ESCREVER UMA ARROW FUNCTION
+
+console.log(raizQuadrada(2));
+console.log(raizQuadrada2(4));
---
+++ arrow-function/script.js
@@ -0,0 +1,11 @@
+let consoleTeste = () => {
+  console.log("ola mundo");
+};
+consoleTeste(); // Chama a função consoleTeste
+
+let soma = (a, b) => {
+  return a + b;
+}; // Função que retorna a soma de dois números
+
+console.log(soma(8, 90));
+// chamando funçao com parametro
